b2c8e8fc913555dc11c288389225117fba64aa83
Nameyray <eparis@atlassian.com>
2022-05-03 09:34:00 +0300

    created a squads data class with a constructor,getters and setters

---
+++ src/main/java/Squads.java
@@ -0,0 +1,25 @@
+public class Squads{
+    private String mName;
+    private String mCause;
+
+    public Squads(String mName, String mCause) {
+        this.mName = mName;
+        this.mCause = mCause;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmCause() {
+        return mCause;
+    }
+
+    public void setmCause(String mCause) {
+        this.mCause = mCause;
+    }
+}
